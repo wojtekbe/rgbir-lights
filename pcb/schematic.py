2852924d1ce89949760182de43dded51a618dfeb
Wojciech Bieganski <wojtekbe@gmail.com>
2017-04-06 21:48:35 +0200

    libs to pcb/lib/, use skidl from repo

--- pcb/schematic.py
+++ pcb/schematic.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python2
 
+import sys
+sys.path.append('../../skidl/skidl')
+
 from skidl import *
+
+lib_search_paths_kicad.append('lib')
 
 gnd = Net('GND')
 vin = Net('+5V')
 pwm_driver = Part('nxp', 'PCA9685PW', footprint='Housings_SSOP:TSSOP-28_4.4x9.7mm_Pitch0.65mm')
+led_drv = Part('macroblock', 'MBI1801', footprint='TODO')
 
 generate_netlist()
